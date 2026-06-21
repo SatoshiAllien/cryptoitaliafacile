48172cdd14591d43edc92ebd893987d16f7588dd
Steven <Krown82@outlook.com>
2026-06-21 16:13:21 +0200

    Fix Edge: cache bust + barra social gold visibile subito

--- js/site-config.js
+++ js/site-config.js
@@ -9,7 +9,7 @@ const SITE_CONFIG = {
   },
   description: 'Notizie crypto e guide spiegate in modo facile e semplice. Per principianti e non solo.',
   year: 2026,
-  assetVersion: '20260621-social-bar',
+  assetVersion: '20260621-edge-fix',
   email: 'info@cryptofacile.com',
 
   teacher: {
